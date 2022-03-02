41cfac282772ea7e04e576709aeec6dd6ee65e3b
Syed Aun Muhammad <aunsyedsyed@gmail.com>
2022-03-02 11:01:13 +0500

    pushing to fyp repo

--- calories_calculator/settings.py
+++ calories_calculator/settings.py
@@ -39,8 +39,9 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'bmr',
+    'api',
     'crispy_forms',
-    'mathfilters'
+    'mathfilters',
 ]
 
 MIDDLEWARE = [
